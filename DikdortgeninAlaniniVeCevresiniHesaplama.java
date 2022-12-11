ce3ac91f5c61a2aadf14c6c09917b77cd0801ae5
reyyhannur <120319109+reyyhannur@users.noreply.github.com>
2022-12-11 21:42:20 +0300

    Add files via upload

---
+++ DikdortgeninAlaniniVeCevresiniHesaplama.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class DikdortgeninAlaniniVeCevresiniHesaplama{
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int kenar1,kenar2;
+        
+        System.out.print("1. Kenarı giriniz: ");
+        kenar1 = scan.nextInt();
+        System.out.print("2. Kenarı giriniz: ");
+        kenar2 = scan.nextInt();
+        
+        System.out.println("Dikdörtgenin alanı = " + (kenar1 * kenar2));
+        System.out.println("Dikdörtgenin çevresi = " + (2 * (kenar1 + kenar2)));
+    }
+ 
+}
+
+
