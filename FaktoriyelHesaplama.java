955a2e7d00f322fde90c46e12c7adb13e9e84e4b
reyyhannur <120319109+reyyhannur@users.noreply.github.com>
2022-12-11 21:47:53 +0300

    Add files via upload

---
+++ FaktoriyelHesaplama.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class FaktoriyelHesaplama{
+    
+    
+    public static void main(String[] args) {
+     
+        int sayi, faktoriyel=1; 
+        Scanner oku=new Scanner(System.in); 
+        System.out.print("Faktöriyeli alınacak sayıyı girin : "); 
+        sayi=oku.nextInt(); 
+        for(int i=2; i<=sayi; i++){ 
+        faktoriyel=i*faktoriyel; 
+        } 
+        System.out.println(sayi+" sayısının faktöriyeli = " + faktoriyel); 
+     
+    }
+    
+    
+}
